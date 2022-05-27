e2f89361493ae7cca80f65b67c5390d40f9a206b
thetime50 <thetime_lx@qq.com>
2022-05-27 18:44:04 +0800

    alarms back

--- simple-ext/background/index.js
+++ simple-ext/background/index.js
@@ -1,6 +1,30 @@
 // background.js
+
+function alarmsTest() {
+    let timeout = 1000 * 60 * 0.05; // 3 minutes in milliseconds
+    // persistent false 没有影响吗
+    window.setTimeout(function () {
+        console.log('Hello,simple-ext setTimeout')
+        alert('Hello,simple-ext setTimeout');
+    }, timeout);
+
+    if (chrome.alarms){
+        chrome.alarms.create({
+            delayInMinutes: 0.05
+        })
+
+        chrome.alarms.onAlarm.addListener(function () {
+            console.log("Hello,simple-ext alarms")
+            alert("Hello,simple-ext alarms")
+        });
+    }else{
+        console.log('chrome.alarms', chrome.alarms)
+    }
+}
+
 chrome.runtime.onInstalled.addListener(function () {
     console.log("插件已被安装2");
+    alarmsTest()
 });
 
 chrome.runtime.onInstalled.addListener(function () {
