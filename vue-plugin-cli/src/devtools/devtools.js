12b2da8a91d6f2470a60e315c94ca3863ec79ccc
thetime50 <thetime_lx@qq.com>
2022-05-30 16:39:09 +0800

    back

--- vue-plugin-cli/src/devtools/devtools.js
+++ vue-plugin-cli/src/devtools/devtools.js
@@ -1,4 +1,4 @@
 import { createApp } from 'vue'
-import App from '../view/devtools.vue'
+import App from './devtools.vue'
 chrome.devtools.panels.create('vue-plugin-cli', '', 'devtools.html')
 createApp(App).mount('#app')
--- vue-plugin-cli/vue.config.js
+++ vue-plugin-cli/vue.config.js
@@ -1,30 +1,75 @@
 const path = require('path')
 const fs = require('fs')
+const { get:loGet } = require('lodash')
 
 // Generate pages object
 const pages = {}
 
-function getEntryFile (entryPath) {
-  let files = fs.readdirSync(entryPath)
-  return files
-}
+// function getPages(){
+//   function getEntryFile (entryPath) {
+//     let files = fs.readdirSync(entryPath)
+//     return files
+//   }
 
-const chromeName = getEntryFile(path.resolve(`src/entry`))
+//   const chromeName = getEntryFile(path.resolve(`src/entry`))
 
-function getFileExtension (filename) {
-  return /[.]/.exec(filename) ? /[^.]+$/.exec(filename)[0] : undefined
-}
-chromeName.forEach((name) => {
-  const fileExtension = getFileExtension(name)
-  const fileName = name.replace('.' + fileExtension, '')
-  pages[fileName] = {
-    entry: `src/entry/${name}`,
-    template: 'public/index.html',
-    filename: `${fileName}.html`
-  }
-})
+//   function getFileExtension (filename) {
+//     return /[.]/.exec(filename) ? /[^.]+$/.exec(filename)[0] : undefined
+//   }
+//   chromeName.forEach((name) => {
+//     const fileExtension = getFileExtension(name)
+//     const fileName = name.replace('.' + fileExtension, '')
+//     pages[fileName] = {
+//       entry: `src/entry/${name}`,
+//       template: 'public/index.html',
+//       filename: `${fileName}.html`
+//     }
+//   })
+// }
+
 
 const isDevMode = process.env.NODE_ENV === 'development'
+
+const manifestPath = path.resolve(`src/manifest.${process.env.NODE_ENV}.json`)
+function getPages(){
+  let fields = [{
+      field: 'background.scripts[0]',
+      name: 'background'
+    },
+    {
+      field: 'browser_action.default_popup',
+      name: 'popup'
+    },
+    {
+      field: 'content_scripts[0].js[0]',
+      name: 'content'
+    },
+    {
+      field: 'options_page',
+      name: 'options'
+    },
+    {
+      field: 'devtools_page',
+      name: 'devtools'
+    },
+  ]
+  const manifest = require(manifestPath)
+  // loGet(manifest, 'field')
+  fields.forEach((fieldObj) => {
+    const field = fieldObj.field
+    const name = fieldObj.name
+    const dist = loGet(manifest, field)
+    if (dist) {
+      pages[name] = {
+        entry: `src/${name}/${name}` + '.js',
+        template: 'public/index.html',
+        filename: `${name}.html`
+      }
+    }
+  })
+}
+
+getPages()
 
 module.exports = {
   pages,
